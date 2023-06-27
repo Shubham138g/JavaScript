736ba225183fc142e1a78b5f5e9feffd453ea77a
shubham <tonygupta275@gmail.com>
2023-06-27 18:23:03 +0530

    intial commit

--- array.js
+++ array.js
@@ -1,4 +1,7 @@
 let fruits=["Mango","Banana","Litchi","Apple"];
-// document.write(fruits)
-let fruits1=document.getElementById("array",fruits)
-fruits1.textContent=fruits;
+let fruits1=document.getElementById("array") //get the id of h1 tag in html document
+let f=fruits1.textContent=fruits;
+
+//array method
+let len= fruits.length;
+fruits1.textContent=len + f;
