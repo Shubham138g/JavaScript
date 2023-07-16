3ff3d918920c24dd01cb6759728711b325613501
shubham <tonygupta275@gmail.com>
2023-07-16 19:47:11 +0530

    intial commit

---
+++ form.js
@@ -0,0 +1,7 @@
+function displaydata(){
+    var text1=document.getElementById("text1")
+    var text2=document.getElementById("text2")
+
+    document.write(text1.value,"<br>")
+    document.write(text2.value)
+}
