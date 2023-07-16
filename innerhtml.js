bdccc379950af398ba8c670670f4cc88f8523770
shubham <tonygupta275@gmail.com>
2023-07-16 20:17:59 +0530

    intial commit

---
+++ innerhtml.js
@@ -0,0 +1,8 @@
+document.getElementById("btn").addEventListener("click",function(){
+    const container=document.getElementById("container")
+    container.innerHTML=`
+    <h1>mai inner html hu
+    </h1>
+    <p>mai thik nhi hu</p>
+    `;
+})
