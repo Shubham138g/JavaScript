829b697febaa782b2aaf14bfc9cf73512d2eaea3
shubham <tonygupta275@gmail.com>
2023-07-16 20:07:06 +0530

    intial commit

--- form.js
+++ form.js
@@ -8,3 +8,22 @@ function displaydata(){
 
 let text=document.querySelector("h2")
 console.log(text)
+
+// ----------------------------------------------------
+document.addEventListener("DOMContentLoaded",function(){
+    const button1=document.getElementById("1")
+    const button2=document.getElementById("2")
+    const button3=document.getElementById("3")
+    const displaybtn=document.getElementById("displaybutton")
+
+
+    button1.addEventListener("click",function(){
+        displaybtn.textContent="button 1 is clicked"
+    })
+    button2.addEventListener("click",function(){
+        displaybtn.textContent="button 2 is clicked"
+    })
+    button3.addEventListener("click",function(){
+        displaybtn.textContent="button 3 is clicked"
+    })
+})
