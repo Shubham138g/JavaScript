2f903887b1fda891708e3e43b218263502f2a500
shubham <tonygupta275@gmail.com>
2023-07-17 20:54:21 +0530

    callback function

--- callback-func.js
+++ callback-func.js
@@ -1,13 +1,51 @@
-function sum(a,b,callback){
+// function sum(a,b,callback){
+//     setTimeout(() => {
+//         console.log("hello i am sum",a+b)
+//         callback();
+//     },1000);
+// }
+
+// function min(x,y)
+// {
+//     console.log("i am min")
+// }
+
+// sum(1,2,min);
+
+// function fetchDataFromServer(callback) {
+//     setTimeout(function () {
+//         const data = ['apple', 'banana', 'orange'];
+//         callback(data);
+//     }, 2000); // Simulating a delay of 2 seconds to fetch the data
+// }
+
+// // Callback function to handle the retrieved data
+// function handleData(data) {
+//     console.log("Data received:", data);
+// }
+
+// // Calling the fetchDataFromServer function with the handleData callback
+// fetchDataFromServer(handleData);
+
+// console.log("Fetching data...");
+
+
+
+
+function obj(callback) {
     setTimeout(() => {
-        console.log("hello i am sum",a+b)
-        callback();
-    },1000);
+        const obj = {
+            name: "shubham",
+            class: "bca"
+        }
+        callback(obj)
+    }, 3000);
 }
 
-function min(x,y)
-{
-    console.log("i am min")
+function display(obj) {
+    console.log("Data recieved", obj)
 }
 
-sum(1,2,min);
+obj(display);
+
+console.log("data aa rha hai...")
