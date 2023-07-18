8b7b824f3f236f6bc125bbd00db9245bdee3815b
shubham <tonygupta275@gmail.com>
2023-07-18 17:30:15 +0530

    intial commit

--- closure.js
+++ closure.js
@@ -1,10 +1,28 @@
-var sum = function (a) {
-    console.log("live viewers", +a)
-    var c = 5;
-    return function (b) {
-        return a + b + c;
+// var sum = function (a) {
+//     console.log("live viewers", +a)
+//     var c = 5;
+//     return function (b) {
+//         return a + b + c;
+//     }
+// }
+
+// var store = sum(2);
+// console.log(store(3))
+
+// ------------------------------------------------
+
+let sum = function (a, b, c) {
+
+    return {
+        getsumTwo: function () {
+            return a + b;
+        },
+        getsumThree: function () {
+            return  a + b + c;
+        }
     }
 }
 
-var store = sum(2);
-console.log(store(3))
+let store = sum(3, 4, 7)
+console.log(store.getsumTwo());
+console.log(store.getsumThree());
