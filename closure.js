52d752b5298836b03af192b3b594ed2d9c66c5bd
shubham <tonygupta275@gmail.com>
2023-07-18 17:31:33 +0530

    intial commit

--- closure.js
+++ closure.js
@@ -11,18 +11,34 @@
 
 // ------------------------------------------------
 
-let sum = function (a, b, c) {
+// let sum = function (a, b, c) {
 
-    return {
-        getsumTwo: function () {
-            return a + b;
-        },
-        getsumThree: function () {
-            return  a + b + c;
-        }
+//     return {
+//         getsumTwo: function () {
+//             return a + b;
+//         },
+//         getsumThree: function () {
+//             return  a + b + c;
+//         }
+//     }
+// }
+
+// let store = sum(3, 4, 7)
+// console.log(store.getsumTwo());
+// console.log(store.getsumThree());
+
+
+
+function outerFunction() {
+    let outerVariable = 'I am from the outer function';
+  
+    function innerFunction() {
+      console.log(outerVariable); // innerFunction has access to outerVariable
     }
-}
-
-let store = sum(3, 4, 7)
-console.log(store.getsumTwo());
-console.log(store.getsumThree());
+  
+    return innerFunction; // Return the inner function without calling it
+  }
+  
+  const closureFunction = outerFunction(); // closureFunction now holds innerFunction with its closure
+  
+  closureFunction(); // Outpu
