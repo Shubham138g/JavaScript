9bf7155ff4bb42b2fc8d3891c8a823707dd3d486
shubham <tonygupta275@gmail.com>
2023-07-19 20:27:33 +0530

    spread and rest operator practice

--- practice.js
+++ practice.js
@@ -1,12 +1,31 @@
 //closure back function
 
-const sum = function (a) {
-    console.log(a)
-    return function (b, c) {
-        return a + b + c;
+// const sum = function (a) {
+//     console.log(a)
+//     return function (b, c) {
+//         return a + b + c;
 
-    }
-}
+//     }
+// }
 
-const store = sum(1);
-console.log(store(1, 3))
+// const store = sum(1);
+// console.log(store(1, 3))
+
+// ---------------------------------------------------------
+
+//spread operator
+
+const emp = [
+    "shubham",
+    "mohan",
+    "kundan kumar sahu",
+    "vikram Gupta",
+    "priyanka Thapa",
+    "Komal verma"
+];
+
+console.log(emp)
+
+const  copyemp={...emp,2:"kundan meri jaan"};
+const {...rest}=emp; //rest operator 
+console.log(copyemp[5],rest)
