eece15c2e11845458550a5a01841bafa808429a4
shubham <tonygupta275@gmail.com>
2024-04-10 22:07:15 +0530

    practice string

--- string.js
+++ string.js
@@ -1,4 +1,15 @@
-var str1="shubham";
-var str2="gupta";
-var str3= str1.split(" ");
-console.log(str3);
+// var str1="shubham";
+// var str2="gupta";
+// var str3= str1.split(" ");
+// console.log(str3);
+
+let arr1=[1,3,4];
+// let arr2=arr1.push(3);
+// console.log(arr1);
+
+// let arr3=arr1.unshift(2,3,4,5,6);
+// console.log(arr1);
+// console.log(5=="5");
+// console.log(5+"5");
+// console.log(5*"5");
+// console.log(5%"5");
