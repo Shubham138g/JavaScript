fa30053ee0e6f850eeb68e7daed735c0a3289d80
shubham <tonygupta275@gmail.com>
2024-04-10 22:30:45 +0530

    practice string

--- string.js
+++ string.js
@@ -3,7 +3,7 @@
 // var str3= str1.split(" ");
 // console.log(str3);
 
-let arr1=[1,3,4];
+let arr1=[1,2,3,4];
 // let arr2=arr1.push(3);
 // console.log(arr1);
 
@@ -20,6 +20,11 @@ let arr1=[1,3,4];
 // console.log(arr1.at(2));
 // console.log(arr1.indexOf(4));
 // console.log(arr1.includes(4)); //return because 4 is in the array
-console.log(arr1.filter((num)=>(num %2 ==0))) //even filter element from the array 
-console.log(arr1.filter((num)=>(num %2 ==1)))//odd filter element from the araay
-
+// console.log(arr1.filter((num)=>(num %2 ==0))) //even filter element from the array 
+// console.log(arr1.filter((num)=>(num %2 ==1)))//odd filter element from the araay
+// console.log(arr1.some((num)=>(num ==2)))
+// console.log(arr1.join("_"));
+// console.log(arr1.toString());
+// console.log(arr1.slice(1,3));
+console.log(arr1.splice(1,2,"5","6"))
+console.log(arr1);
