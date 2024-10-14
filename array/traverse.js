4f0a3db05a9eeb7a27d238d75d7686e74a2e1980
shubham <tonygupta275@gmail.com>
2024-10-14 19:36:28 +0530

    array travers

--- array/traverse.js
+++ array/traverse.js
@@ -2,3 +2,9 @@ let number=[1,2,3,4,5,6,7,8,9,10];
 for(let i=0;i<=number.length-1;i++){
     console.log(number[i]);
 }
+
+let fruits=['mango','banana','grapes','apple']
+for(let j=0;j<=fruits.length-1;j++){
+    console.log(fruits[j]);
+    
+}
