03fe2c79b0a7d000ba7d08544cb06ceb9f15a98a
shubham <tonygupta275@gmail.com>
2024-10-14 19:55:03 +0530

    array deletions

---
+++ array/deletion.js
@@ -0,0 +1,11 @@
+let number=[1,44,2,56,35,10];
+
+function deleteElement(position){
+    for(let i=position;i<=number.length-2;i++){
+        number[i]=number[i+1];
+        
+    }
+    number.length = number.length - 1;
+}
+deleteElement(1)
+console.log(number);
